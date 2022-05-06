3cbbf453dda339499296b0368e719a165404acb8
huangjie <1364045672@qq.com>
2022-05-06 17:41:51 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,13 @@
 </template>
 
 <script>
-
 export default {
   name: 'App',
   components: {
+    
   }
 }
 </script>
 
-<style>
-html,body{
-  margin: 0;
-  padding: 0;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+<style lang="less" scoped>
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false 
+  lintOnSave: false,
+  //配置代理服务器
+  devServer: {
+    proxy: 'https://lianghj.top:8888/api/private/v1/'
+  }
 })
